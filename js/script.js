b68db95eca97b93d06e01da920505e6a07171307
Louise Webster <louise_webster@mail.com>
2021-06-14 16:50:57 +0100

    Completed nav section and mobile hero section

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
-const hamburgerBtnOpen = document.getElementById('hamburger-btn-open');
-const hamburgerBtnClose = document.getElementById('hamburger-btn-close');
+const hamburgerBtn = document.getElementById('hamburger-btn');
 const overlay = document.getElementById('overlay');
 const navLinks = document.getElementById('nav-links');
+const body = document.getElementById('body');
 
-hamburgerBtnOpen.addEventListener('click', function(){
-        overlay.classList.remove('hide');
-        hamburgerBtnOpen.classList.add('hide');
-        hamburgerBtnClose.classList.remove('hide');
-        navLinks.classList.remove('hide');
-});
-
-hamburgerBtnClose.addEventListener('click', function(){
-    overlay.classList.add('hide');
-    hamburgerBtnOpen.classList.remove('hide');
-    hamburgerBtnClose.classList.add('hide');
-    navLinks.classList.add('hide');
+hamburgerBtn.addEventListener('click', function(){
+    hamburgerBtn.classList.toggle('close');
+    overlay.classList.toggle('show');
+    navLinks.classList.toggle('show');
+    body.classList.toggle('no-scroll');
 });
